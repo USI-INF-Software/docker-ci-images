484d1c556e5e6a62ea976e55ca4d6c553223376c
Jesper Findahl <jesper.findahl@gmail.com>
2018-02-09 11:16:31 +0100

    Adds scripts to build and push the docker images that have been changed in last commit Updates .gitlab-ci.yml to run scripts in CI. Fixes typo in polymer-ci/Dockerfile.

---
+++ ci-jobs/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+DOCKER_FILE="Dockerfile"
+
+# get the list of changed files in last commit
+FILE_DIFF=$(git diff-tree --no-commit-id --name-only -r HEAD)
+
+# for each folder that contains a Dockerfile
+for F in $FILE_DIFF
+do
+  if [[ $F = *$DOCKER_FILE ]]; then
+    DIR=${F%/$DOCKER_FILE}
+    NAME=${DIR#*/}
+    TAG="${DOCKER_ID_USER}/${NAME}"
+    docker build -t $TAG $DIR
+
+    # if we could not build then exit with error
+    if [ $? -ne 0 ]; then
+      exit 1
+    fi
+
+  fi
+done
---
+++ ci-jobs/push.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+DOCKER_FILE="Dockerfile"
+
+# get the list of changed files in last commit
+FILE_DIFF=$(git diff-tree --no-commit-id --name-only -r HEAD)
+
+# login to dockerhub
+echo "$DOCKER_ID_PASSWORD" | docker login -u="$DOCKER_ID_USER" --password-stdin
+
+# if we could not login then exit with error
+if [ $? -ne 0 ]; then
+  exit 1
+fi
+
+# for each folder that contains a Dockerfile
+for F in $FILE_DIFF
+do
+  if [[ $F = *$DOCKER_FILE ]]; then
+    DIR=${F%/$DOCKER_FILE}
+    NAME=${DIR#*/}
+    TAG="${DOCKER_ID_USER}/${NAME}"
+    docker push $TAG
+
+    # if we could not push then exit with error
+    if [ $? -ne 0 ]; then
+      exit 1
+    fi
+
+  fi
+done
+
+# logout from dockerhub
+docker logout
