7d0eacebe3e6391981915c83da869f9e39244707
Jesper Findahl <jesper.findahl@gmail.com>
2018-02-09 15:57:59 +0100

    Updates ci-jobs scripts

--- ci-jobs/build.sh
+++ ci-jobs/build.sh
@@ -3,7 +3,7 @@
 DOCKER_FILE="Dockerfile"
 
 # get the list of changed files in last commit
-FILE_DIFF=$(git diff-tree --no-commit-id --name-only -r HEAD)
+FILE_DIFF=$(git diff-tree --no-commit-id --name-only -r HEAD~1..HEAD)
 
 # for each folder that contains a Dockerfile
 for F in $FILE_DIFF
@@ -11,7 +11,7 @@ do
   if [[ $F = *$DOCKER_FILE ]]; then
     DIR=${F%/$DOCKER_FILE}
     NAME=${DIR#*/}
-    TAG="${DOCKER_ID_USER}/${NAME}"
+    TAG="${DOCKER_ORG}/${NAME}"
     docker build -t $TAG $DIR
 
     # if we could not build then exit with error
--- ci-jobs/push.sh
+++ ci-jobs/push.sh
@@ -3,7 +3,7 @@
 DOCKER_FILE="Dockerfile"
 
 # get the list of changed files in last commit
-FILE_DIFF=$(git diff-tree --no-commit-id --name-only -r HEAD)
+FILE_DIFF=$(git diff-tree --no-commit-id --name-only -r HEAD~1..HEAD)
 
 # login to dockerhub
 echo "$DOCKER_ID_PASSWORD" | docker login -u="$DOCKER_ID_USER" --password-stdin
@@ -19,7 +19,7 @@ do
   if [[ $F = *$DOCKER_FILE ]]; then
     DIR=${F%/$DOCKER_FILE}
     NAME=${DIR#*/}
-    TAG="${DOCKER_ID_USER}/${NAME}"
+    TAG="${DOCKER_ORG}/${NAME}"
     docker push $TAG
 
     # if we could not push then exit with error
