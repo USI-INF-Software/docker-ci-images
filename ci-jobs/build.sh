b1ed7b9347645f0993ddf5c6869279fe16ba9d13
Jesper Findahl <jesper.findahl@gmail.com>
2021-11-05 14:58:23 +0100

    #8: update nrp77 docker image

--- ci-jobs/build.sh
+++ ci-jobs/build.sh
@@ -5,14 +5,6 @@ DOCKER_FILE="Dockerfile"
 # get the list of changed files in last commit
 FILE_DIFF=$(git diff-tree --no-commit-id --name-status -r HEAD~1..HEAD | grep "^[ACMR]")
 
-NRP77_IMAGE_DIR="nrp77-spacy-ci"
-
-function download_nrp77_environment {
-  wget --header "PRIVATE-TOKEN: ${NRP77_NLP_ANALYSIS_REPO_TOKEN}" \
-    https://gitlab.dev.si.usi.ch/api/v4/projects/297/repository/files/environment.yml/raw \
-    --output-document="${NRP77_IMAGE_DIR}/environment.yml"
-}
-
 # for each folder that contains a Dockerfile
 # folder name cannot contain space
 for F in $FILE_DIFF
@@ -22,10 +14,6 @@ do
     DIR=${F%/"$DOCKER_FILE"}
     NAME=${DIR#*/}
     TAG="${DOCKER_ORG}/${NAME}"
-
-    if [[ "$DIR" = "$NRP77_IMAGE_DIR" ]]; then
-      download_nrp77_environment
-    fi
 
     echo "Building ${TAG} from ${DIR}"
     docker build -t "$TAG" "$DIR"
